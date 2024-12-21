8dbc84f000db3de27811340203184f412db32e9b
Sufferal <botnari_ciprian@mail.ru>
2024-12-21 18:36:13 +0200

    feat: add initial setup

---
+++ ui/src/App.jsx
@@ -0,0 +1,12 @@
+import UserSelect from './components/UserSelect';
+
+function App() {
+
+  return (
+    <div className="app__wrapper">
+      <UserSelect />
+    </div>
+  )
+}
+
+export default App
